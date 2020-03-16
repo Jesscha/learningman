ff7d7e4bd14c29c54bf77c0ff2ba6357b40a0702
jessecha <jessecha222@gmail.com>
2020-03-16 14:02:22 +0900

    add GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -124,7 +124,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `160750308-1`,
       },
     },
     `gatsby-plugin-feed`,
