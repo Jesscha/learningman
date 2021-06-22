738ccebb934f1fddee43d54d0f808848fc3f30da
jesse cha <jeses.cha@navercorp.com>
2021-06-23 00:02:11 +0900

    #84 태그 분리

--- src/components/nav.js
+++ src/components/nav.js
@@ -7,7 +7,7 @@ import './nav.css';
 
 const Nav = ({tag, location}) => {
     const [move, setMove] = useState(false);
-    let current
+    let current;
     const navRef = useRef(null)
 
     const scrollToNav = (ref) => {
@@ -20,23 +20,22 @@ const Nav = ({tag, location}) => {
 
     useEffect(()=>{
         current = document.getElementById(tag) 
-        const changeFirstCategory  = ()=>{
-            document.getElementById('all').innerText = tag
-            document.getElementById('all').parentElement.classList.add('navBox-active')
-        }
-        const changeFirstCategoryToAll = () =>{
-            document.getElementById('all').parentElement.classList.add('navBox-active')
-        }
-        current?
-        current.parentElement.classList.add('navBox-active') : 
-        location && location.pathname === "/" ?
-        changeFirstCategoryToAll()
-        :
-        changeFirstCategory()
+        // const changeFirstCategory  = ()=>{
+        //     document.getElementById('all').innerText = tag
+        //     document.getElementById('all').parentElement.classList.add('navBox-active')
+        // }
+        // const changeFirstCategoryToAll = () =>{
+        //     document.getElementById('all')?parentElement.classList.add('navBox-active')
+        // }
+        // current?current.parentElement.classList.add('navBox-active') : 
+        // location && location.pathname === "/" ?
+        // changeFirstCategoryToAll()
+        // :
+        // changeFirstCategory()
 
         const navWrapper = document.querySelector('.navWrapper')
         const navPosition = navWrapper.offsetTop
-
+        
     },[]
     )
     
@@ -49,11 +48,26 @@ const Nav = ({tag, location}) => {
     }}>
 <div className='navWrapper' ref={navRef}>
     <div className="navBox" >
-        <Link id={"all"} to={'/'} >
-                All
+        <Link id={"nav-Eddy"} to={'/tags/eddy'} >
+                Eddy
         </Link>
     </div>
-
+    <div className="navBox" >
+        <Link id={"nav-jesse"} to={'/tags/jesse'} >
+                Jesse
+        </Link>
+    </div>
+    <div className="navBox" >
+        <Link id={"nav-Kay"} to={'/tags/kay'} >
+                Kay
+        </Link>
+    </div>
+    <div className="navBox" >
+        <Link id={"nav-Robbie"} to={'/tags/robbie'} >
+                Robbie
+        </Link>
+    </div>
+{/* ~
     <div className="navBox" >
          <Link id={'essay'} to={'tags/essay'}>
                 Essay
@@ -61,7 +75,7 @@ const Nav = ({tag, location}) => {
     </div>
     <div className="navBox">
         <Link id={'challenge'} to={'tags/challenge'}>Challenge</Link>
-    </div>
+    </div> */}
     <div className="navBox navBox-end">
          1
     </div>
