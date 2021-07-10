7d2135fe986aa0689e36497d673fdd4fc80ab71c
jesse cha <jeses.cha@navercorp.com>
2021-07-10 19:17:03 +0900

    저자별로 컨텐츠 앞뒤로 이동

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,18 +1,22 @@
 const _ = require("lodash")
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
+// const { AUTHORS } = require("./src/constants/constant")
 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
   const blogPost = path.resolve(`./src/templates/blog-post.js`)
   const tagTemplate = path.resolve(`src/templates/tags.js`)
-  const result = await graphql(
-    `
+  const AUTHORS = ["eddy", "jesse", "kay", "robbie"]
+  AUTHORS.forEach(async author => {
+    const result = await graphql(
+      `
       {
         allMarkdownRemark(
           sort: { fields: [frontmatter___date], order: DESC }
           limit: 1000
+          filter: { frontmatter: { tags: { regex: "/${author}/" } } }
         ) {
           edges {
             node {
@@ -28,48 +32,49 @@ exports.createPages = async ({ graphql, actions }) => {
         }
       }
     `
-  )
+    )
 
-  if (result.errors) {
-    throw result.errors
-  }
-
-  // Create blog posts pages.
-  const posts = result.data.allMarkdownRemark.edges
-
-  posts.forEach((post, index) => {
-    const previous = index === posts.length - 1 ? null : posts[index + 1].node
-    const next = index === 0 ? null : posts[index - 1].node
-    // console.log(post.node.fields.slug)
-
-    createPage({
-      path: post.node.fields.slug,
-      component: blogPost,
-      context: {
-        slug: post.node.fields.slug,
-        previous,
-        next,
-      },
-    })
-  })
-
-  let tags = []
-
-  _.each(posts, edge => {
-    if (_.get(edge, "node.frontmatter.tags")) {
-      tags = tags.concat(edge.node.frontmatter.tags)
+    if (result.errors) {
+      throw result.errors
     }
-  })
 
-  tags = _.uniq(tags)
+    // Create blog posts pages.
+    const posts = result.data.allMarkdownRemark.edges
+    // 현재 목표, 전, 후를 특정 저자가 쓴 작품으로 연결하기
+    // 가능한 방법, posts 여러개를 둔다.
+    posts.forEach(async (post, index) => {
+      const previous = index === posts.length - 1 ? null : posts[index + 1].node
+      const next = index === 0 ? null : posts[index - 1].node
 
-  tags.forEach(tag => {
-    createPage({
-      path: `/tags/${_.kebabCase(tag)}/`,
-      component: tagTemplate,
-      context: {
-        tag,
-      },
+      createPage({
+        path: post.node.fields.slug,
+        component: blogPost,
+        context: {
+          slug: post.node.fields.slug,
+          previous,
+          next,
+        },
+      })
+    })
+
+    let tags = []
+
+    _.each(posts, edge => {
+      if (_.get(edge, "node.frontmatter.tags")) {
+        tags = tags.concat(edge.node.frontmatter.tags)
+      }
+    })
+
+    tags = _.uniq(tags)
+
+    tags.forEach(async tag => {
+      createPage({
+        path: `/tags/${_.kebabCase(tag)}/`,
+        component: tagTemplate,
+        context: {
+          tag,
+        },
+      })
     })
   })
 }
