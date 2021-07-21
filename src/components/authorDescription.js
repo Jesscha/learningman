f3032c4fb4bc8d4b0692622db5dd67212d43e106
jesse cha <jeses.cha@navercorp.com>
2021-07-21 20:23:57 +0900

    #88 새탭으로 링크연결

--- src/components/authorDescription.js
+++ src/components/authorDescription.js
@@ -137,7 +137,10 @@ const AuthorDescription = ({ tag, isSmall, isBorderTop, isBorderBottom }) => {
     if (author === AUTHORS[2]) {
       return (
         <div className="author-desc">
-          스타트업과 컨설팅을 거쳐 현재 정유사에서 해외영업을 담당하고 있습니다. IT 산업과 에너지 산업, 블록체인 및 기술 기반 프로덕트에 관심이 많습니다. 어릴 적부터 꿈은 대통령이지만 대통령이 되지 않아도 행복할 수 있는 사람으로 성장하려고 노력합니다.
+          스타트업과 컨설팅을 거쳐 현재 정유사에서 해외영업을 담당하고 있습니다.
+          IT 산업과 에너지 산업, 블록체인 및 기술 기반 프로덕트에 관심이
+          많습니다. 어릴 적부터 꿈은 대통령이지만 대통령이 되지 않아도 행복할 수
+          있는 사람으로 성장하려고 노력합니다.
           <div className="author-link-wrapper">
             <AuthorLink link="https://fb.com/withoutyoung" linkLogoType="fb" />
           </div>
@@ -191,7 +194,7 @@ const AuthorDescription = ({ tag, isSmall, isBorderTop, isBorderBottom }) => {
 
     return (
       <div className="author-link">
-        <a href={link}>
+        <a href={link} target={"blank"}>
           <img src={_link} alt="" />
         </a>
       </div>
